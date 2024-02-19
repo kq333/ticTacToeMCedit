1c0e31a66aa7e1092ee45ba6d56617527bcc0d91
kq333 <afugowski@yandex.com>
2024-02-19 22:51:00 +0100

    delete console.log

--- src/Ts/userMove.ts
+++ src/Ts/userMove.ts
@@ -1,34 +1,33 @@
-import {  aiNextMove } from './aiNextMove.ts';
-import {  nextTurnAi } from './aiMove.ts';
-import { isUserWin } from './isUserWin.ts';
-import { aiWinTheTure } from './isAiWin.ts';
+import { aiNextMove } from './aiNextMove.ts'
+import { nextTurnAi } from './aiMove.ts'
+import { isUserWin } from './isUserWin.ts'
+import { aiWinTheTure } from './isAiWin.ts'
+import { userChoosenCharacter } from './characterDraw.ts'
 
-export let userMoves: number[] = [];
-export let userTurnNum: number = 0;
-export let nextTurnUser: boolean = false;
+export let userMoves: number[] = []
+export let userTurnNum: number = 0
+export let nextTurnUser: boolean = false
 
 export function setNextTurnUser(value: boolean): void {
-    nextTurnUser = value;
-};
+  nextTurnUser = value
+}
 
 export function userMove(): void {
+  if (!aiWinTheTure) {
+    const getGridElem: NodeListOf<HTMLElement> =
+      document.querySelectorAll('.grid__item')
 
-    if (!aiWinTheTure) {
-        const getGridElem: NodeListOf<HTMLElement> = document.querySelectorAll('.grid__item');
-
-        getGridElem.forEach((elem: HTMLElement, idx: number) => {
-
-            elem.addEventListener('click', ()=> {
-
-                if (!userMoves.includes(idx) && nextTurnAi) {
-                    elem.classList.add('grid__item--user');
-                    userMoves.push(idx);
-                    userTurnNum++;
-                    nextTurnUser = !nextTurnUser;
-                    isUserWin();
-                    aiNextMove();
-                }
-            })
-        })
-    }
-};
+    getGridElem.forEach((elem: HTMLElement, idx: number) => {
+      elem.addEventListener('click', () => {
+        if (!userMoves.includes(idx) && nextTurnAi) {
+          elem.style.backgroundImage = `url('${userChoosenCharacter[0].img}')`
+          userMoves.push(idx)
+          userTurnNum++
+          nextTurnUser = !nextTurnUser
+          isUserWin()
+          aiNextMove()
+        }
+      })
+    })
+  }
+}

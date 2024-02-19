135ea078c7e4c9193a5a09c1831f4ffca19e1e7f
kq333 <afugowski@yandex.com>
2024-02-19 21:27:46 +0100

    added id to characters objects

--- src/Ts/charactersImage.ts
+++ src/Ts/charactersImage.ts
@@ -3,41 +3,49 @@ export const charactersImage = [
     name: 'Baraka',
     img: './src/assets/baraka.webp',
     gif: './src/assets/baraka-gif.gif',
+    id: 0,
   },
   {
     name: 'Jade',
     img: './src/assets/Jade.webp',
     gif: './src/assets/jade-gif.gif',
+    id: 1,
   },
   {
     name: 'Kitana',
     img: './src/assets/Kitana.webp',
     gif: './src/assets/kitana-gif.gif',
+    id: 2,
   },
   {
     name: 'Kung Lao',
     img: './src/assets/KungLao.webp',
     gif: './src/assets/kungLao-gif.gif',
+    id: 3,
   },
   {
     name: 'Mileena',
     img: './src/assets/Mileena.webp',
     gif: './src/assets/mileena-gif.gif',
+    id: 4,
   },
   {
     name: 'Noob Saibot',
     img: './src/assets/noobSaibot.webp',
     gif: './src/assets/noobSaibot-gif.gif',
+    id: 5,
   },
   {
     name: 'Scorpion',
     img: './src/assets/Scorpion.webp',
     gif: './src/assets/Skorpion-gif.gif',
+    id: 6,
   },
 
   {
     name: 'Sub-Zero',
     img: './src/assets/Sub-Zero.jpg',
     gif: './src/assets/Sub-Zero-gif.gif',
+    id: 7,
   },
 ]

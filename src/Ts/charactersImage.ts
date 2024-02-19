d531459349cf273f284ab687065c34eb3acc37b5
kq333 <afugowski@yandex.com>
2024-02-19 19:55:47 +0100

    created file wiht image as helper

---
+++ src/Ts/charactersImage.ts
@@ -0,0 +1,43 @@
+export const charactersImage = [
+  {
+    name: 'Baraka',
+    img: './src/assets/baraka.webp',
+    gif: './src/assets/baraka-gif.gif',
+  },
+  {
+    name: 'Jade',
+    img: './src/assets/Jade.webp',
+    gif: './src/assets/jade-gif.gif',
+  },
+  {
+    name: 'Kitana',
+    img: './src/assets/Kitana.webp',
+    gif: './src/assets/kitana-gif.gif',
+  },
+  {
+    name: 'Kung Lao',
+    img: './src/assets/KungLao.webp',
+    gif: './src/assets/kungLao-gif.gif',
+  },
+  {
+    name: 'Mileena',
+    img: './src/assets/Mileena.webp',
+    gif: './src/assets/mileena-gif.gif',
+  },
+  {
+    name: 'Noob Saibot',
+    img: './src/assets/noobSaibot.webp',
+    gif: './src/assets/noobSaibot-gif.gif',
+  },
+  {
+    name: 'Scorpion',
+    img: './src/assets/Scorpion.webp',
+    gif: './src/assets/Skorpion-gif.gif',
+  },
+
+  {
+    name: 'Sub-Zero',
+    img: './src/assets/Sub-Zero.jpg',
+    gif: './src/assets/Sub-Zero-gif.gif',
+  },
+]

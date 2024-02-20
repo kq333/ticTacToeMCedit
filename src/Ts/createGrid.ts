9f5ffd655a9d67836b10f9e667cedb23f3e89ac2
kq333 <afugowski@yandex.com>
2024-02-21 00:11:04 +0100

    created grid container elem

--- src/Ts/createGrid.ts
+++ src/Ts/createGrid.ts
@@ -1,10 +1,17 @@
 export function createGrid(): void {
-  const getSectionElem: HTMLElement | null = document.querySelector('.section')
-  const getGrid: HTMLElement | null = document.createElement('div')
-  getGrid.classList.add('grid')
-  getSectionElem?.appendChild(getGrid)
+  const getSectionElem: HTMLElement | null = document.querySelector(
+    '.section__container',
+  )
+  const createElemDiv: HTMLElement | null = document.createElement('div')
+  createElemDiv.classList.add('grid')
+  getSectionElem?.appendChild(createElemDiv)
 
-  if (!getGrid) {
+  const getGridElem = document.querySelector('.grid')
+  const createElemContainer = document.createElement('div')
+  createElemContainer.classList.add('grid__container')
+  getGridElem?.appendChild(createElemContainer)
+
+  if (!createElemDiv) {
     console.error('Grid element not found!')
     return
   }
@@ -12,6 +19,6 @@ export function createGrid(): void {
   for (let i: number = 0; i < 9; i++) {
     const createElem: HTMLDivElement = document.createElement('div')
     createElem.classList.add('grid__item')
-    getGrid.appendChild(createElem)
+    createElemContainer.appendChild(createElem)
   }
 }

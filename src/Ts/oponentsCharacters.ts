18f6b6b38ee7acf15cb9b46bdb2e9abadda61eaf
kq333 <afugowski@yandex.com>
2024-02-21 00:11:40 +0100

    created card container elem

--- src/Ts/oponentsCharacters.ts
+++ src/Ts/oponentsCharacters.ts
@@ -8,25 +8,28 @@ export function oponentsCharacters(
 
   console.log(opponentsArray)
 
-  const getElem: HTMLElement | null = document.querySelector('.section')
+  const getElem: HTMLElement | null = document.querySelector('.section__container')
   const createElem = document.createElement('div')
   createElem.classList.add('card-opponents')
   getElem?.appendChild(createElem)
 
+  const getElemParent = document.querySelector('.card-opponents')
+  const createElemDiv = document.createElement('div')
+  createElemDiv.classList.add('card-opponents__container')
+  getElemParent?.appendChild(createElemDiv)
+
   const getCardElem: HTMLElement | null =
-    document.querySelector('.card-opponents')
+    document.querySelector('.card-opponents__container')
 
   if (getCardElem !== null) {
     const htmlElements = opponentsArray.map(
       (elem: CharacterImage, index: number): string => {
-        const label = index % 2 === 0 ? 'User' : 'Mighty AI'
+        const label = index % 2 === 0 ? 'Player' : 'Mighty Ai'
         return `
-          <div class="card-opponents__container">
             <div class="card-opponents__item">
-              <p> ${label} </p>
+              <p  class="card-opponents__text"> ${label} </p>
               <img src='${elem.img}' alt='${elem.name}' class="card-opponents__img">
             </div>
-          </div>
       `
       },
     )

3fd7159603d77f993d492766c74ec4552daa5cc2
kq333 <afugowski@yandex.com>
2024-02-21 19:44:34 +0100

    added import img from assets

--- src/Ts/charactersImage.ts
+++ src/Ts/charactersImage.ts
@@ -1,51 +1,68 @@
+import baracaImg from '../assets/Baraka.webp'
+import baracaGif from '../assets/baraka-gif.gif'
+import jadeImg from '../assets/Jade.webp'
+import jadeGif from '../assets/jade-gif.gif'
+import kitanaImg from '../assets/Kitana.webp'
+import kitanaGif from '../assets/kitana-gif.gif'
+import kungLaoImg from '../assets/KungLao.webp'
+import kungLaoGif from '../assets/kungLao-gif.gif'
+import mileenaImg from '../assets/Mileena.webp'
+import mileenaGif from '../assets/mileena-gif.gif'
+import noobSaibotImg from '../assets/noobSaibot.webp'
+import noobSaibotGif from '../assets/noobSaibot-gif.gif'
+import scorpionImg from '../assets/Scorpion.webp'
+import scorpionGif from '../assets/Skorpion-gif.gif'
+import subZeroImg from '../assets/Sub-Zero.jpg'
+import subZeroGif from '../assets/Sub-Zero-gif.gif'
+
 export const charactersImage = [
   {
     name: 'Baraka',
-    img: './src/assets/baraka.webp',
-    gif: './src/assets/baraka-gif.gif',
+    img: baracaImg,
+    gif: baracaGif,
     id: 0,
   },
   {
     name: 'Jade',
-    img: './src/assets/Jade.webp',
-    gif: './src/assets/jade-gif.gif',
+    img: jadeImg,
+    gif: jadeGif,
     id: 1,
   },
   {
     name: 'Kitana',
-    img: './src/assets/Kitana.webp',
-    gif: './src/assets/kitana-gif.gif',
+    img: kitanaImg,
+    gif: kitanaGif,
     id: 2,
   },
   {
     name: 'Kung Lao',
-    img: './src/assets/KungLao.webp',
-    gif: './src/assets/kungLao-gif.gif',
+    img: kungLaoImg,
+    gif: kungLaoGif,
     id: 3,
   },
   {
     name: 'Mileena',
-    img: './src/assets/Mileena.webp',
-    gif: './src/assets/mileena-gif.gif',
+    img: mileenaImg,
+    gif: mileenaGif,
     id: 4,
   },
   {
     name: 'Noob Saibot',
-    img: './src/assets/noobSaibot.webp',
-    gif: './src/assets/noobSaibot-gif.gif',
+    img: noobSaibotImg,
+    gif: noobSaibotGif,
     id: 5,
   },
   {
     name: 'Scorpion',
-    img: './src/assets/Scorpion.webp',
-    gif: './src/assets/Skorpion-gif.gif',
+    img: scorpionImg,
+    gif: scorpionGif,
     id: 6,
   },
 
   {
     name: 'Sub-Zero',
-    img: './src/assets/Sub-Zero.jpg',
-    gif: './src/assets/Sub-Zero-gif.gif',
+    img: subZeroImg,
+    gif: subZeroGif,
     id: 7,
   },
 ]

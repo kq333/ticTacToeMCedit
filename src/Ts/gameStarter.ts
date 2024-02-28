e14551fa77c592a4ff08c46a271418ae07c00d7b
kq333 <afugowski@yandex.com>
2024-02-28 12:46:35 +0100

    added class fage-in to component

--- src/Ts/gameStarter.ts
+++ src/Ts/gameStarter.ts
@@ -6,38 +6,36 @@ export function gameStarter() {
 
   const introElem = `
     <div class="intro-block">
-        <div class="intro-block__container">
-            <img src="${imgStarter}" alt="fighter gif">
-            <div class="intro-block__btn-container">
-              <button class="intro-block__btn-reload">Play</button>
-            </div>
+      <div class="intro-block__container">
+        <img src="${imgStarter}" alt="fighter gif">
+        <div class="intro-block__btn-container">
+          <button class="intro-block__btn-reload">Play</button>
         </div>
+      </div>
     </div>
-    `
+  `
 
   if (getParentElem) {
     getParentElem.insertAdjacentHTML('beforeend', introElem)
 
-    const getBtnStart: HTMLElement | null = document.querySelector(
-      '.intro-block__btn-reload',
-    )
-
-    const getIntroBlock: HTMLElement | null =
-      document.querySelector('.intro-block')
-
-    const getPageContainer: HTMLElement | null =
-      document.querySelector('.page__container')
+    const getBtnStart: HTMLElement | null = document.querySelector('.intro-block__btn-reload')
+    const getIntroBlock: HTMLElement | null = document.querySelector('.intro-block')
+    const getPageContainer: HTMLElement | null = document.querySelector('.page__container')
 
     if (getPageContainer) {
       getPageContainer.style.display = 'none'
     }
 
     getBtnStart?.addEventListener('click', () => {
-      getIntroBlock?.remove()
-      if (getPageContainer) {
-        getPageContainer.style.display = 'block'
-        createGridCharacters()
-      }
+      getIntroBlock?.classList.add('fade-out');
+      setTimeout(() => {
+        getIntroBlock?.remove();
+        if (getPageContainer) {
+          getPageContainer.style.display = 'block'
+          createGridCharacters()
+        }
+      }, 900);
     })
   }
 }
+
